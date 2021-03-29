52b58172026e53bebd04d0f2498600d0ee0ad075
Franciscocoo <francois.jullion99@gmail.com>
2021-03-29 08:48:05 +0200

    Debut Partie.java

--- Pacman/Logic/Partie.java
+++ Pacman/Logic/Partie.java
@@ -1,5 +1,31 @@
 package Pacman.Logic;
 
-public class Partie {
-    
+public class Partie implements IPartie{
+
+	/*
+	 * 
+	 */
+	private Grille Grille;
+	
+	/*
+	 * 
+	 */
+	private int Score;
+	
+	/*
+	 * 
+	 */
+	private int Niveau;
+	
+	/*
+	 * 
+	 */
+	private EStatutPartie EtatPartie;
+	
+	/*
+	 * 
+	 */
+	public Partie() {
+		Grille Grille = DataForLogic
+	}
 }
