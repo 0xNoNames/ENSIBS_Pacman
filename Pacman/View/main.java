d3b55ac6db9f69ae90fe0b6314c04959613a617b
Arthur Pêtre <44344292+arthur-petre@users.noreply.github.com>
2021-04-03 19:28:08 +0200

    Ajout du test dans le main

--- Pacman/View/main.java
+++ Pacman/View/main.java
@@ -1,12 +1,20 @@
 package Pacman.View;
 
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
 public class main {
 
     entreeClavier keyboard;
 
     public static void main(String[] args) {
-        System.out.println("Démarrage");
+        System.out.println("Démarrage...");
         entreeClavier test = new entreeClavier();
-        System.out.println("Sortie");
+
+        Fenetre fenetre = new Fenetre();
+
+        fenetre.setVisible(true);
+
+        fenetre.addKeyListener(new entreeClavier());
+        System.out.println("Done");
     }
 }
