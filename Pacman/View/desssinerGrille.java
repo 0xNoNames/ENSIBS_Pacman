fe6de2f7042227d5ff3d868ed04dc8973db2cfbb
Petre Arthur <petre.e2000707@etud.univ-ubs.fr>
2021-04-17 18:49:00 +0200

    fix offset fruits

--- Pacman/View/desssinerGrille.java
+++ Pacman/View/desssinerGrille.java
@@ -50,28 +50,28 @@ public class desssinerGrille {
                         // Selon l'objet dans la case on l'affiche.
                         switch (caseJ.getObjet().getClass().getSimpleName()) {
                         case "Cerise":
-                            g2d.drawImage(data.getFruitSprites()[0], (i * 8) + 4, (8 * j) + 32, null);
+                            g2d.drawImage(data.getFruitSprites()[0], (i * 8), (8 * j) + 24, null);
                             break;
                         case "Fraise":
-                            g2d.drawImage(data.getFruitSprites()[1], (i * 8) + 4, (8 * j) + 32, null);
+                            g2d.drawImage(data.getFruitSprites()[1], (i * 8) , (8 * j) + 24, null);
                             break;
                         case "Orange":
-                            g2d.drawImage(data.getFruitSprites()[2], (i * 8) + 4, (8 * j) + 32, null);
+                            g2d.drawImage(data.getFruitSprites()[2], (i * 8), (8 * j) + 24, null);
                             break;
                         case "Pomme":
-                            g2d.drawImage(data.getFruitSprites()[3], (i * 8) + 4, (8 * j) + 32, null);
+                            g2d.drawImage(data.getFruitSprites()[3], (i * 8), (8 * j) + 24, null);
                             break;
                         case "Melon":
-                            g2d.drawImage(data.getFruitSprites()[4], (i * 8) + 4, (8 * j) + 32, null);
+                            g2d.drawImage(data.getFruitSprites()[4], (i * 8), (8 * j) + 24, null);
                             break;
                         case "Galboss":
-                            g2d.drawImage(data.getFruitSprites()[5], (i * 8) + 4, (8 * j) + 32, null);
+                            g2d.drawImage(data.getFruitSprites()[5], (i * 8), (8 * j) + 24, null);
                             break;
                         case "Cloche":
-                            g2d.drawImage(data.getFruitSprites()[6], (i * 8) + 4, (8 * j) + 32, null);
+                            g2d.drawImage(data.getFruitSprites()[6], (i * 8), (8 * j) + 24, null);
                             break;
                         case "Clé":
-                            g2d.drawImage(data.getFruitSprites()[7], (i * 8) + 4, (8 * j) + 32, null);
+                            g2d.drawImage(data.getFruitSprites()[7], (i * 8), (8 * j) + 24, null);
                             break;
                         case "PetiteGomme":
                             g2d.drawImage(data.getGommesSprites()[0], (i * 8) + 1, (8 * j) + 29, null);
